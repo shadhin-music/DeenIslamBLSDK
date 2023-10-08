9d0d23a1e6fdfbe06ec33966d82d808c8dc3f25c
MD Azizur Rahman <azizur@shadhinmusic.com>
2023-10-08 11:33:43 +0600

    framework add

---
+++ DeenSDK_BL.xcframework/ios-arm64/DeenIslamSDK.framework/Headers/NewObjectiveSDK.h
@@ -0,0 +1,16 @@
+//
+//  NewObjectiveSDK.h
+//  DeenIslamSDK
+//
+//  Created by MacBook Pro on 24/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewObjectiveSDK : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
